07d055d13deffb65c6ea7a938fb3bf56b4d947f0
Isaak <isaakdosreis@gmail.com>
2024-05-28 20:40:24 -0300

    update function to generate table

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -62,19 +62,21 @@ class Produto {
             let td_acoes = tr.insertCell();
 
             td_id.innerText = this.arrayProdutos[i].id;
+            td_id.classList.add('center');
             td_produto.innerText = this.arrayProdutos[i].nomeProduto;
             td_preco.innerText = this.arrayProdutos[i].preco;
 
-            let imgEdit = document.createElement('img');
-            imgEdit.src = 'img/editing.png';
-            imgEdit.setAttribute("onclick", "produto.editar(" + JSON.stringify(this.arrayProdutos[i]) + ")");
+            let imgEditar = document.createElement('img');
+            imgEditar.src = 'img/editing.png';
+            imgEditar.setAttribute("onclick", "produto.editar(" + JSON.stringify(this.arrayProdutos[i]) + ")");
 
-            let imgDelete = document.createElement('img');
-            imgDelete.src = 'img/trash.png';
-            imgDelete.setAttribute("onclick", "produto.deletar(" + this.arrayProdutos[i].id + ")");
+            let imgDeletar = document.createElement('img');
+            imgDeletar.src = 'img/trash.png';
+            imgDeletar.setAttribute("onclick", "produto.deletar(" + this.arrayProdutos[i].id + ")");
 
-            td_acoes.appendChild(imgEdit);
-            td_acoes.appendChild(imgDelete);
+            td_acoes.appendChild(imgEditar);
+            td_acoes.appendChild(imgDeletar);
+            td_acoes.classList.add('center');
         }
     }
 
